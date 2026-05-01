a0faecef217796a5d3a32565f94005eb5daabd2a
Akhmad-Fadhlan <73178458+Akhmad-Fadhlan@users.noreply.github.com>
2026-05-01 14:36:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
     },
   },
   server: {
